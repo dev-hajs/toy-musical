14fae2f2f39417cecd9c4c8cc5ea385c69ad259f
하지수 <dev.hajs@gmail.com>
2021-08-11 22:35:52 +0900

    first commit

---
+++ src/main/java/com/project/musical/HajsApplication.java
@@ -0,0 +1,13 @@
+package com.project.musical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajsApplication.class, args);
+	}
+
+}
